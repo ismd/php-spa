d5af71b8a5d394b9b285a8b84ccf4e66cb9089c1
Vladimir Kosteley <zzismd@gmail.com>
2013-07-22 14:41:20 +0700

    Fixed include_path

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
 // Определяем директорию с исходниками
 define('APPLICATION_PATH', realpath(dirname(__FILE__)) . '/../application');
 
-set_include_path(implode('/', array(
-    realpath(APPLICATION_PATH . '../lib'),
+set_include_path(implode(':', array(
+    realpath(APPLICATION_PATH . '/../lib'),
     get_include_path(),
 )));
 
