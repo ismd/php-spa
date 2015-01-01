288041f9ed4cd7b8f1b559360d20494aa3467af1
Vladimir Kosteley <zzismd@gmail.com>
2015-01-01 14:34:56 +0600

    Not it's possible not to copy/create application.ini for config

--- lib/php-spa/PsConfig.php
+++ lib/php-spa/PsConfig.php
@@ -10,7 +10,8 @@ class PsConfig extends PsSingleton {
      * Путь к главному конфигурационному файлу
      * @var string
      */
-    const FILENAME = '/configs/application.ini';
+    const FILENAME         = '/configs/application.ini';
+    const FILENAME_EXAMPLE = '/configs/application.example.ini';
 
     /**
      * Объект конфига
@@ -23,16 +24,21 @@ class PsConfig extends PsSingleton {
      * @throws Exception
      */
     protected function __construct() {
-        $filename = APPLICATION_PATH . self::FILENAME;
+        $filename        = APPLICATION_PATH . self::FILENAME;
+        $filenameExample = APPLICATION_PATH . self::FILENAME_EXAMPLE;
 
-        if (!is_readable($filename)) {
+        if (!is_readable($filename) && !is_readable($filenameExample)) {
             throw new Exception("Can't read config file");
         }
 
-        $config = parse_ini_file($filename, true);
+        $config = [];
 
-        if (!$config) {
-            throw new Exception("Can't read config file");
+        if (is_readable($filename)) {
+            $config = $this->_parse($filename);
+        }
+
+        if (is_readable($filenameExample)) {
+            $config = $this->_parse($filenameExample, $config);
         }
 
         // Преобразуем все элементы к объектам
@@ -41,6 +47,29 @@ class PsConfig extends PsSingleton {
         });
 
         $this->_config = (object)$config;
+    }
+
+    protected function _parse($filename, $oldConfig = []) {
+        $config = parse_ini_file($filename, true);
+
+        if (!$config) {
+            throw new Exception("Can't read config file");
+        }
+
+        foreach ($config as $i => $section) {
+            if (!isset($oldConfig[$i])) {
+                $oldConfig[$i] = $section;
+                continue;
+            }
+
+            foreach ($section as $j => $item) {
+                if (!isset($oldConfig[$i][$j])) {
+                    $oldConfig[$i][$j] = $item;
+                }
+            }
+        }
+
+        return $oldConfig;
     }
 
     public function __get($name) {
